87dbb57c425ccc00050a402e82895fc5da369092
prathamkundan <2020csb1114@iitrpr.ac.in>
2023-01-02 19:29:10 +0530

    book appointment skeleton

--- gcp-project/src/components/BookAppointment.js
+++ gcp-project/src/components/BookAppointment.js
@@ -1,74 +1,94 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { useAuth } from '../contexts/AuthContext';
+import { db } from '../firebase';
 import './BookAppointment.css'
 
 export default function BookAppointment() {
+
+  const { currentUser } = useAuth();
+  const symptomRef = useRef();
+  const dateRef = useRef();
+  const timeRef = useRef();
+
+  async function addAppointment(e) {
+    e.preventDefault();
+
+    db.collection("appointments").add({
+      patientid: currentUser.uid,
+      doctorid: "some_id",
+      time: timeRef.current.value,
+      date: dateRef.current.value,
+    })
+
+  }
+
   return (
     <div className="container mt-4 p-4">
-        <div className="row">
-          <div className="col-md-6">
-            <h2 className="text-center my-4">
-              Book Appointment
-            </h2>
-            <form>
-              <div className="form-group row">
-                <label className="col-sm-4 col-lg-4">
-                  Patient Name
-                </label>
-                <div className="col-sm-8 col-lg-8">
-                  <input type="text" id="patient-name" className="form-control" placeholder="Name" required />
-                </div>
+      <div className="row">
+        <div className="col-md-6">
+          <h2 className="text-center my-4">
+            Book Appointment
+          </h2>
+          <form>
+            <div className="form-group row">
+              <label className="col-sm-4 col-lg-4">
+                Patient Name
+              </label>
+              <div className="col-sm-8 col-lg-8">
+                <input type="text" id="patient-name" className="form-control" placeholder="Name" required />
               </div>
-              {/**/}
-              <br />
-              <div className="form-group row">
-                <label className="col-sm-4 col-lg-4">
-                  Contact
-                </label>
-                <div className="col-sm-8 col-lg-8">
-                  <input type="tel" id="contact" className="form-control" placeholder={123} required />
-                </div>
+            </div>
+            {/**/}
+            <br />
+            <div className="form-group row">
+              <label className="col-sm-4 col-lg-4">
+                Contact
+              </label>
+              <div className="col-sm-8 col-lg-8">
+                <input type="tel" id="contact" className="form-control" placeholder={123} required />
               </div>
-              {/**/}
-              <br />
-              <div className="form-group row">
-                <label className="col-sm-4 col-lg-4">
-                  Date
-                </label>
-                <div className="col-sm-8 col-lg-8">
-                  <input type="date" id="date" className="form-control" />
-                </div>
+            </div>
+            {/**/}
+            <br />
+            <div className="form-group row">
+              <label className="col-sm-4 col-lg-4">
+                Date
+              </label>
+              <div className="col-sm-8 col-lg-8">
+                <input ref={dateRef} type="date" id="date" className="form-control" />
               </div>
-              {/**/}
-              <br />
-              <div className="form-group row">
-                <label className="col-sm-4 col-lg-4">
-                  Time
-                </label>
-                <div className="col-sm-8 col-lg-8">
-                  <input type="time" id="time" className="form-control" />
-                </div>
+            </div>
+            {/**/}
+            <br />
+            <div className="form-group row">
+              <label className="col-sm-4 col-lg-4">
+                Time
+              </label>
+              <div className="col-sm-8 col-lg-8">
+                <input ref={timeRef} type="time" id="time" className="form-control" />
               </div>
-              {/**/}
-              <br />
-              <div className="form-group row">
-                <label className="col-sm-4 col-lg-4">
-                  Reason of Visit
-                </label>
-                <div className="col-sm-8 col-lg-8">
-                  <textarea id="symptoms" className="form-control" required defaultValue={""} />
-                </div>
+            </div>
+            {/**/}
+            <br />
+            <div className="form-group row">
+              <label className="col-sm-4 col-lg-4">
+                Reason of Visit
+              </label>
+              <div className="col-sm-8 col-lg-8">
+                <textarea ref={symptomRef} id="symptoms" className="form-control" required defaultValue={""} />
               </div>
-              {/**/}
-              <br />
-              <div className="form-group row justify-content-end">
-                <div className="col-sm-5">
-                  <button type="submit" className="btn btn-form">
-                    Confirm
-                  </button>
-                </div>
+            </div>
+            {/**/}
+            <br />
+            <div className="form-group row justify-content-end">
+              <div className="col-sm-5">
+                <button type="submit" className="btn btn-form" onClick={addAppointment()}>
+                  Confirm
+                </button>
               </div>
-            </form>
-          </div>
-        </div></div>
+            </div>
+          </form>
+        </div>
+      </div></div>
   )
 }
