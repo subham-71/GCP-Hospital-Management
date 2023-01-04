f8215af0bafadb5ba513f5b92f584eec2f71f9f8
SukhmeetSingh <78161187+SukhmeetSingh2002@users.noreply.github.com>
2023-01-04 21:20:14 +0530

    Some good Changes

--- gcp-project/src/components/PatientList.js
+++ gcp-project/src/components/PatientList.js
@@ -46,7 +46,7 @@ export default function PatientList() {
   return (
     <div>
       <div id="fixed-div">
-        <div id="left-div">Name :<div id="name">Dr. Ashima Goyal </div></div>
+        <div id="left-div">Name :<div id="name">Dr. Someone </div></div>
         <div id="right-div">Hospital :<div id="hospital">Fortis</div></div>
       </div>
       <div className="container1">
@@ -87,55 +87,10 @@ export default function PatientList() {
           </tbody>
         </table>
         <br />
-        <div id="add"><button className=" btn btn-primary" onClick={() => document.getElementById("registration-form").style.display = 'block'}>Add Patient</button></div>
         <div className="container2">
           <div className="row">
             <div className="col-md-4">
-              <form className="patient-form" id="registration-form">
-                <div className="form-group">
-                  <label htmlFor="patient-name">Patient Name</label>
-                  <input type="text" className="form-control" id="patient-name" placeholder="Enter patient name" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="age">Age</label>
-                  <input type="number" className="form-control" id="age" placeholder="Enter age" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="gender">Gender</label>
-                  <select className="form-control" id="gender">
-                    <option>Male</option>
-                    <option>Female</option>
-                    <option>Other</option>
-                  </select>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="height">Height</label>
-                  <input type="number" className="form-control" id="height" placeholder="Enter height in cm" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="weight">Weight</label>
-                  <input type="number" className="form-control" id="weight" placeholder="Enter weight in kg" />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="blood-group">Blood Group</label>
-                  <select className="form-control" id="blood-group">
-                    <option>A+</option>
-                    <option>A-</option>
-                    <option>B+</option>
-                    <option>B-</option>
-                    <option>AB+</option>
-                    <option>AB-</option>
-                    <option>O+</option>
-                    <option>O-</option>
-                  </select>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="last-visit">Last Visit</label>
-                  <input type="date" className="form-control" id="last-visit" />
-                </div>
-                <button type="submit" id="submit" className="btn btn-success">Submit</button>
-                <button type="cancel" id="cancel" className="btn btn-warning" onClick={() => document.getElementById("registration-form").style.display = 'none'}>Cancel</button>
-              </form></div></div></div>
+              </div></div></div>
       </div>
     </div>
   )
