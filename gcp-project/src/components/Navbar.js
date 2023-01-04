db703f6a7b97cc63be57490db311b9680e04bd2e
SukhmeetSingh <78161187+SukhmeetSingh2002@users.noreply.github.com>
2023-01-05 01:24:03 +0530

    Added navbar signout

--- gcp-project/src/components/Navbar.js
+++ gcp-project/src/components/Navbar.js
@@ -5,8 +5,9 @@ const Navbar = () => {
   return (
 
     <div className="d-flex flex-column flex-shrink-0" id="navbar">
-      <div className="container" style={{ height: '10rem' }}>
-      </div>
+      {/* <div className="container" style={{ height: '10rem' }}>
+      </div> */}
+      {/* <div id="nav_layout">
       <ul className="nav nav-flush flex-column mb-auto text-center">
         <li className="nav-item">
           <a href="#" className="nav-link active py-3" aria-current="page" title data-bs-toggle="tooltip" data-bs-placement="right" data-bs-original-title="Home">
@@ -31,17 +32,18 @@ const Navbar = () => {
         <li>
         </li>
       </ul>
-      <div className="dropdown border-top">
+      </div> */}
+      <div className="dropdown " id="nav_layout">
         <a href="#" className="d-flex align-items-center justify-content-center p-3 link-light text-decoration-none dropdown-toggle" id="dropdownUser3" data-bs-toggle="dropdown" aria-expanded="false">
           <img src="https://github.com/mdo.png" alt="mdo" className="rounded-circle" width={24} height={24} />
         </a>
         <ul className="dropdown-menu text-small shadow" aria-labelledby="dropdownUser3">
-          <li><a className="dropdown-item" href="#">Reset</a></li>
+          {/* <li><a className="dropdown-item" href="#">Reset</a></li>
           <li><a className="dropdown-item" href="#">Settings</a></li>
-          <li><a className="dropdown-item" href="#">Profile</a></li>
-          <li>
+          <li><a className="dropdown-item" href="#">Profile</a></li> */}
+          {/* <li>
             <hr className="dropdown-divider" />
-          </li>
+          </li> */}
           <li><a className="dropdown-item" href="#">Sign out</a></li>
         </ul>
       </div>
